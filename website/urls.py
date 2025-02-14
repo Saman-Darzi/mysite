45529afc09f73b7b2394832058eff0cfa24cebba
Saman-Darzi <samandarzi857@gmail.com>
2025-02-14 18:56:46 +0330

    created render html files
    added templates folder and html files to app website

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from website.views import *
 
 urlpatterns = [
-    path('Home',index_view),
+    path('',index_view),
     path('about',about_view),
     path('contact',contact_view)
     
